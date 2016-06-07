ec33a51c38ccd11f510d04b7f95ac8eac54b23b3
ljorgens <leejorgensen22@gmail.com>
2016-06-06 23:10:24 -0400

    add enviornmental variables

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 var express = require('express');
 fs = require('fs');
 request = require('request');
@@ -6,15 +7,15 @@ firebase = require("firebase");
 
 firebase.initializeApp({
 	serviceAccount: {
-		projectId: "testnewfirebase-50f80",
-		clientEmail: "realtestaccount@testnewfirebase-50f80.iam.gserviceaccount.com",
-		privateKey: "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCN9nQwMZQKry3i\n4g6dH2pKPUWsenhPOPZ74JqT+KQB7bQg0rLKwq7Rpyj1vXORgXwdGongKA0JHYvD\ncnhpAEvr8DaNZiv4TnR2IR6210JOILE4ZND+Wr0kWpjmD7rJtTkIRYwDrvBxkK5S\nriSgGXHKpfLV4yowDIHEvovD6cVHXLE3szg+am3HukuzQd4zWXxgY2E0uxAORlCS\nErJkGXKXcCJdOHYErApzp60m2eKOEkjkDO1u2teEe4g3rtxaYCoE8ZwjhvDWfEb5\ngB91uo4cTttltjkmzg6N2XfFuDJJOUJsP5jfrQS7ctU6TRRydUHz6U5JujHL+ElR\n+JzErinbAgMBAAECggEAKFVpVXgAk7iJ1PhdJo8SZ91YulRhckDA5xk9UeVG07Vx\nhBHY8nq1qKNRo+Abwa+ET9CGc8VtTrWyS+luBIctCu/I0AvDWl0QAbAPReuKzx/C\nuj8Kkfm922JSJqs5aq2ucocv7Kv7bIiqvprzlPklt2nGwLY1+kjTZc6tbxAwGMHz\nbXl6UoLpfCYGYlriB/WlaO6ZYT4pCB5AGBDsTrj4jo3XHSHwgHZmaOHKqiHn+uCm\nEHqYEtKvfnB399tFfcuAiBd4TsdzTl5o2D5//+osSsNjcIqeMSLZ0afeIjcPzjA3\nNYWrh871QUonAkTjBthAxzCqbcNYCp5JQmpX8AfJAQKBgQDf3KHngqeJPZpoOTLy\nlpDGEpgdPJ9aHQXncoJ5p+Qxx9A+dQthLqDCfd/NIEeYiKZYTOn6+4gnDRFBPUEV\nvLYeKGWMaD64BbHym32LrGjuf6+3o6fgY5PvX45pnp/YZtToV1etsecTK12Zde9A\nTtVC8cCujM2x/J/1sEoi1C7xuwKBgQCiV92RdldSwv7pd0qHe66EDu9dfSWGAr/w\ntUy88nhbRR15nc8StposMpogNJgW657aFQKaRkypvd0gwKPRKQHJKJrLZba7g0eC\nFhYKwvbvOyzwx2rPY6/hMeZO2k5iD7UQlb8LDiwN05/e7inzb064IMVWHyzDXxGp\nB1EsTzOWYQKBgCJ9SpBxh8BUNmLSVTAc1Gc/3CG9AkwO5qb29HUr0bN9tm6496zc\nUzZD0GQ5Ee7FNuOpze62LA7gK4Vim5Fuxpw02xhbwzX04qqfHCOVx4DfOUJQu3+N\nt/AMR9Or9bKQfULimgaPvysr5jrLebXtr1umBWqfFN3ULwzaQUHGcQkbAoGBAIey\nYiXAgWVQULrdV5K/szP3N+UxQjgt/Do7kRUup6PCtcx+OCQwNdZTxbi9vZMLCBDK\nFw7VTCdl6HwECeo/GJsIMbtqHb/HD/KQe4QrSzTUhdANIB6R+OWHTDcFCxe0gWiH\ngtS+19wG1E8HTFGK7IXD/q7qCASWZrCeyR78ZOaBAoGANqKHFpwOM37zuHmJqlUH\np1WuMeuRp5UiRg1hYOkWhP2OLUYqnkDcfBq3i6UFdjvXOJplzVt8W5YCbS2HtkKx\nzZp+oxHo8Qy6LPshpd90G5jBPEckHRCFBWQfPQ6q78y9kqADRraVstIQ3+KYK4g7\nvnnjOFOsJSk07CoqU1k1LxM=\n-----END PRIVATE KEY-----\n"
+		projectId: process.env.PROJECT_ID,
+		clientEmail: process.env.CLIENT_EMAIL,
+		privateKey: process.env.PRIVATE_KEY
 	},
-	databaseURL: "https://testnewfirebase-50f80.firebaseio.com/"
+	databaseURL: process.env.FIREBASE_URL
 })
 
 var db = firebase.database();
-gameDayDetail = "http://data.wnba.com/data/1m/v2015/json/mobile_teams/wnba/2016/scores/gamedetail/1011600003_gamedetail.json"
+gameDayDetail = process.env.URL
 statsRef = db.ref("stormPlayerStats")
 playerRef = db.ref("players")
 currentSelectedPlayersRef = db.ref("currentSelectedPlayers")
